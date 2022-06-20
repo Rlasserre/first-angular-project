ed069c440c244e0586f7de012bc1e1fcc00e59a7
Rafael Lasserre <80365790+Rlasserre@users.noreply.github.com>
2022-06-20 11:12:26 -0300

    Update component
    entendendo melhor como utilizar templates e containers com o ngcontent.

--- fundamentos-angular/src/app/card/card.component.ts
+++ fundamentos-angular/src/app/card/card.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent implements OnInit {
+export class CardComponent {
 
-  constructor() { }
+  @Input() titulo = "";
 
-  ngOnInit(): void {
-  }
+  @Input() cor = "#CCC";
 
 }
